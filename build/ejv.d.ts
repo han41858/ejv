02cb56aaf73cf33e79846410ef87c5f2429b5b59
han41858 <han41858@gmail.com>
2020-07-02 00:22:55 +0900

    .gitignore /build folder added

--- build/ejv.d.ts
+++
@@ -1,2 +0,0 @@
-import { EjvError, Options, Scheme } from './interfaces';
-export declare const ejv: (data: object, schemes: Scheme[], options?: Options | undefined) => null | EjvError;
--- build/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export { ejv } from './ejv';
-export { DataType, NumberFormat, StringFormat, ErrorMsg, ErrorType } from './constants';
-export { EjvError, Scheme, Options } from './interfaces';
